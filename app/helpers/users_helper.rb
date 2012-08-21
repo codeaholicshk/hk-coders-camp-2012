401e8dd5f049e5646b05c32b9c20df36a278cb45
Matthew Rudy Jacobs <MatthewRudyJacobs@gmail.com>
2012-08-22 01:04:24 +0800

    user picture when logged in, and user pictures on the coder list

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,9 @@
 # Helper methods defined here can be accessed in any controller or view in the application
 
 HkCodersCamp2012.helpers do
-  # def simple_helper_method
-  #  ...
-  # end
+
+  def gravatar_tag(user, options={})
+    size = options.delete(:size) || 40
+    image_tag("https://secure.gravatar.com/avatar/#{user.gravatar_id}?s=#{size}")
+  end
 end
