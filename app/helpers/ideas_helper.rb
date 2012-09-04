d184783f6c52a9fa785325f268adb9c3d79ad644
3dd13 <tatonlto@gmail.com>
2012-09-05 00:01:59 +0800

    change the idea note html

--- app/helpers/ideas_helper.rb
+++ app/helpers/ideas_helper.rb
@@ -1,7 +1,12 @@
 # Helper methods defined here can be accessed in any controller or view in the application
 
 HkCodersCamp2012.helpers do
-  # def simple_helper_method
-  #  ...
-  # end
+  def up_vote_link(idea)
+    content = "▲#{idea.voters.size}"
+    if current_account
+      link_to content, url(:ideas, :up_vote, :id => idea.id), :method => :post
+    else
+      content
+    end
+  end
 end
